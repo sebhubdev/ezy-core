bd0626582dfe61101c5a3a88a5943d75c39fd060
Sebastian <neumann@antadis.com>
2023-12-19 15:03:31 +0100

    fix hmr

--- src/server/server-hmr.js
+++ src/server/server-hmr.js
@@ -9,13 +9,16 @@ require("dotenv").config();
 const app = express();
 
 const webpackCompiler = webpack({
-  entry: ["webpack-hot-middleware/client", `./src/index.js`],
+  entry: [
+    "webpack-hot-middleware/client",
+    path.resolve(`node_modules/ezy-core/src/index.js`),
+  ],
   mode: "development",
   devtool: "source-map",
   module: {
     rules: [
       {
-        exclude: /node_modules/,
+        // exclude: /node_modules/,
         test: /.js$/,
         use: {
           loader: "babel-loader",
@@ -62,17 +65,45 @@ const webpackCompiler = webpack({
   ],
   resolve: {
     alias: {
-      components: path.resolve("./src/components/"),
-      pages: path.resolve("./src/pages/"),
-      routes: path.resolve("./src/routes/"),
-      assets: path.resolve("./src/assets/"),
-      modules: path.resolve("./src/modules/"),
-      layout: path.resolve("./src/layout/"),
-      services: path.resolve("./src/services/"),
-      dataServices: path.resolve("./src/dataServices/"),
-      api: path.resolve("./src/api/"),
-      hooks: path.resolve("./src/hooks/"),
-      i18n: path.resolve("./src/i18n/"),
+      components: [
+        path.resolve(`./src/components/`),
+        path.resolve(__dirname, `../components/`),
+      ],
+      pages: [
+        path.resolve(`./src/pages/`),
+        path.resolve(__dirname, `../pages/`),
+      ],
+      routes: [
+        path.resolve(`./src/routes/`),
+        path.resolve(__dirname, `../routes/`),
+      ],
+      assets: [
+        path.resolve(`./src/assets/`),
+        path.resolve(__dirname, `../assets/`),
+      ],
+      modules: [
+        path.resolve(`./src/modules/`),
+        path.resolve(__dirname, `../modules/`),
+      ],
+      layout: [
+        path.resolve(`./src/layout/`),
+        path.resolve(__dirname, `../layout/`),
+      ],
+      services: [
+        path.resolve(`./src/services/`),
+        path.resolve(__dirname, `../services/`),
+      ],
+      dataServices: [
+        path.resolve(`./src/dataServices/`),
+        path.resolve(__dirname, `../dataServices/`),
+      ],
+
+      api: [path.resolve(`./src/api/`), path.resolve(__dirname, `../api/`)],
+      hooks: [
+        path.resolve(`./src/hooks/`),
+        path.resolve(__dirname, `../hooks/`),
+      ],
+      i18n: [path.resolve(`./src/i18n/`), path.resolve(__dirname, `../i18n/`)],
     },
     extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
@@ -84,7 +115,7 @@ const webpackHotMiddleware = require("webpack-hot-middleware");
 app.use(webpackDevMiddleware(webpackCompiler));
 app.use(webpackHotMiddleware(webpackCompiler));
 
-app.use("*", express.static(`./src/server`));
+app.use("*", express.static(`node_modules/ezy-core/src/server`));
 
 // const PORT = process.env.PORT ;
 
