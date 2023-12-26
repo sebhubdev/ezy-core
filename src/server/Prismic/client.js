1d35d3fa11c3250da79f5162602dac36bfa3ba91
Sebastian <neumann@antadis.com>
2023-12-26 19:19:47 +0100

    some fixes

--- src/server/Prismic/client.js
+++ src/server/Prismic/client.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 import * as prismic from "@prismicio/client";
 
-const repoName = "chiro-bressuire";
+const repoName = process.env.PRISMIC_REPOSITORY;
 const client = prismic.createClient(repoName, { fetch });
 
 export default client;
