4a14cbede09f08bee07feb0c8ba8b9c14f73dc6f
Sebastian <neumann@antadis.com>
2023-12-28 16:59:52 +0100

    adding prosmic access token

--- src/server/Prismic/client.js
+++ src/server/Prismic/client.js
@@ -2,6 +2,9 @@ import fetch from "node-fetch";
 import * as prismic from "@prismicio/client";
 
 const repoName = process.env.PRISMIC_REPOSITORY;
-const client = prismic.createClient(repoName, { fetch });
+const client = prismic.createClient(repoName, {
+  accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+  fetch,
+});
 
 export default client;
